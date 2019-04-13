2032883764143049bb0e625ee18e4d696eeafb13
JaSv <orcwarrior_9@hotmail.com>
2019-04-13 14:24:27 -0700

    Adding the API support for adding transactions to the pool and looking at it

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const Blockchain = require("./platform/blockchain");
 const DataManager = require("./data_manager");
+const DataPool = require("./data_manager/data-pool");
 
 const app = express();
 const blockchain = new Blockchain();
+const dataManager = new DataManager();
+const dataPool = new DataPool();
 
 const DEFAULT_PORT = 3000;
 
@@ -22,6 +25,46 @@ app.post("/api/mine", (req, res) => {
   //pubsub.broadcastChain();
 
   res.redirect("/api/blocks");
+});
+
+app.get("/api/data-pool-map", (req, res) => {
+  res.json(dataPool.segmentStatusMap);
+});
+
+app.post("/api/create-segment-status", (req, res) => {
+  const { segmentTraffic } = req.body;
+
+  let segmentStatus = dataPool.existingSegmentStatus({
+    segmentAddress: dataManager.publicKey
+  });
+
+  try {
+    //TODO: code what happens if the segment already exists (update)
+    //if (segmentStatus) {
+    //  segmentStatus.update({ senderWallet: wallet, recipient, amount });
+    //} else {
+    //TODO: for now segmentTraffic is hardcoded, but this data needs to be provided by the actual vehicles
+    //connected to the segment
+    segmentStatus = dataManager.createSegmentStatus({
+      segmentTraffic: { V1: 10, V2: 20 }
+    });
+    //}
+  } catch (error) {
+    return res.status(400).json({ type: "error", message: error.message });
+  }
+
+  dataPool.setSegmentStatus(segmentStatus);
+
+  //pubsub.broadcastTransaction(transaction);
+
+  res.status(200).json({ type: "success", segmentStatus });
+});
+
+app.get("/api/data-manager-info", (req, res) => {
+  const address = dataManager.publicKey;
+  res.json({
+    address
+  });
 });
 
 const PORT = DEFAULT_PORT;
